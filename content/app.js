21f60cff1462ad3fc839de1cce0d3566f1fbc095
Andriy Rybak <arybak@universe.dart.spb>
2016-05-25 13:32:12 +0300

    minor updates

---
+++ content/app.js
@@ -0,0 +1,51 @@
+var initModule = (function() {
+    var btn = document.getElementById('do');
+    var text = document.getElementById('text');
+
+    text.value = window.localStorage.getItem('text');
+
+    btn.addEventListener('click', function(e) {
+        window.localStorage.setItem('text', text.value);
+    });
+}());
+
+var workerTest = (function() {
+
+    var blob = new Blob([
+        document.querySelector('#worker1').textContent
+    ], {
+        type: "text/javascript"
+    });
+
+    var myWorker = new Worker(window.URL.createObjectURL(blob));
+
+    myWorker.postMessage('some message');
+
+    myWorker.addEventListener('message', function(e) {
+        alert(e.data);
+    });
+
+}());
+
+
+var notifsDemo = (function() {
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification("Hi there!");
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function(permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification("Hi there!");
+            }
+        });
+    }
+}());
