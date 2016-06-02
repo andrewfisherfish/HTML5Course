4f031e1b38c1bd19ac1b496879f7bf5aabbf2e79
Andriy Rybak <Andriy.Rybak@dataart.com>
2016-06-02 19:53:49 +0300

    'updates'

--- content/app.js
+++ content/app.js
@@ -1,3 +1,4 @@
+//http://www.html5rocks.com/en/tutorials/offline/storage/#apis
 var initModule = (function() {
     var btn = document.getElementById('do');
     var text = document.getElementById('text');
@@ -9,6 +10,7 @@ var initModule = (function() {
     });
 }());
 
+//http://www.html5rocks.com/en/tutorials/workers/basics/
 var workerTest = (function() {
 
     var blob = new Blob([
@@ -27,7 +29,7 @@ var workerTest = (function() {
 
 }());
 
-
+//https://developer.mozilla.org/en-US/docs/Web/API/notification
 var notifsDemo = (function() {
     if (!("Notification" in window)) {
         alert("This browser does not support desktop notification");
@@ -36,7 +38,7 @@ var notifsDemo = (function() {
     // Let's check whether notification permissions have already been granted
     else if (Notification.permission === "granted") {
         // If it's okay let's create a notification
-        var notification = new Notification("Hi there!");
+        //var notification = new Notification("Hi there!");
     }
 
     // Otherwise, we need to ask the user for permission
@@ -44,7 +46,7 @@ var notifsDemo = (function() {
         Notification.requestPermission(function(permission) {
             // If the user accepts, let's create a notification
             if (permission === "granted") {
-                var notification = new Notification("Hi there!");
+                //var notification = new Notification("Hi there!");
             }
         }, function() {
             console.log('access default');
@@ -54,16 +56,128 @@ var notifsDemo = (function() {
     }
 }());
 
-
+//http://www.html5rocks.com/en/tutorials/dnd/basics/#toc-dataTransfer
 var draggerDropper = (function() {
-    if (Modernizr.draganddrop) {
-        // Browser supports HTML5 DnD.
-    } else {
-        // Fallback to a library solution.
+
+    function handleСontainerDragTarget(e) {
+        switch (e.type) {
+            case 'dragenter':
+                this.classList.add('enter');
+                break;
+            case 'dragleave':
+                this.classList.remove('enter');
+                break;
+            case 'dragover':
+                this.classList.add('enter');
+                e.preventDefault();
+                break;
+            case 'drop':
+                this.classList.remove('enter');
+                var elId = e.dataTransfer.getData('text/html');
+                var el = document.getElementById(elId);
+
+                if (el) {
+                    this.appendChild(el);
+
+                    break;
+                }
+
+                if (e.dataTransfer.files.length > 0) {
+                    e.preventDefault();
+                }
+                break;
+        }
     }
 
-    document.addEventListener('dragstart', function(event) {
-        event.dataTransfer.setData('text', 'Customized text');
-        event.dataTransfer.effectAllowed = 'copy';
-    }, false);
+    function handleDragTarget(e) {
+        switch (e.type) {
+            case 'dragenter':
+                this.classList.add('enter');
+                break;
+            case 'dragleave':
+                this.classList.remove('enter');
+                break;
+            case 'dragover':
+                this.classList.add('enter');
+                e.preventDefault();
+                break;
+            case 'drop':
+                this.classList.remove('enter');
+
+                if (e.dataTransfer.files.length > 0) {
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        this.style.backgroundImage = 'url(' + e.target.result + ')';
+                    }.bind(this);
+
+                    reader.readAsDataURL(e.dataTransfer.files[0]);
+                }
+
+                e.preventDefault();
+                break;
+        }
+    }
+
+    function handleDragSource(e) {
+        switch (e.type) {
+            case 'dragstart':
+                var msg = e.target.id;
+                this.classList.add('start');
+                e.dataTransfer.setData('text/html', msg);
+                break;
+            case 'dragend':
+                this.classList.remove('start');
+                break;
+        }
+    }
+
+    var cols = document.querySelectorAll('.dragable-container');
+
+    [].forEach.call(cols, function(col) {
+        col.addEventListener('dragstart', handleDragSource, false);
+        col.addEventListener('dragend', handleDragSource, false);
+        col.addEventListener('dragover', handleDragTarget, false);
+        col.addEventListener('dragenter', handleDragTarget, false);
+        col.addEventListener('dragleave', handleDragTarget, false);
+        col.addEventListener('drop', handleDragTarget, false);
+    });
+
+    var containers = document.querySelectorAll('.dropable-container');
+
+    [].forEach.call(containers, function(container) {
+        container.addEventListener('dragover', handleСontainerDragTarget, false);
+        container.addEventListener('dragenter', handleСontainerDragTarget, false);
+        container.addEventListener('dragleave', handleСontainerDragTarget, false);
+        container.addEventListener('drop', handleСontainerDragTarget, false);
+    });
+
+}());
+
+
+var fileWriter = (function() {
+    var opt_errorHandler = function() {
+
+    };
+    
+    window.requestFileSystem(window.TEMPORARY, 1024 * 1024, function(fs) {
+
+        // fs.root is a DirectoryEntry object.
+        fs.root.getFile('log.txt', {
+            create: true
+        }, function(fileEntry) {
+
+            fileEntry.createWriter(function(writer) { // writer is a FileWriter object.
+
+                writer.onwrite = function(e) {};
+                writer.onerror = function(e) {};
+
+                var bb = new BlobBuilder();
+                bb.append('Hello World!');
+
+                writer.write(bb.getBlob('text/plain'));
+
+            }, opt_errorHandler);
+        });
+
+    }, opt_errorHandler);
 }());
